82aad4bb258def2375e2c0723285b3c1a14cfc5f
Paul Jolly <paul@myitcv.org.uk>
2017-05-02 19:15:33 +0100

    reactVet docs

--- cmd/reactVet/reactVet.go
+++ cmd/reactVet/reactVet.go
@@ -1,3 +1,7 @@
+// reactVet is a vet program used to check the correctness of myitcv.io/react based packages.
+//
+//For more information see https://github.com/myitcv/react/wiki/reactVet
+//
 package main
 
 import (
